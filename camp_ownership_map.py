f70925b3951ca4438182c110101285bb7eae8dbf
Tim Norris <tibben@ocf.berkeley.edu>
2025-07-24 14:39:20 -0400

    feat: refactor for gdsc

--- camp_ownership_map.py
+++ camp_ownership_map.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+import json
         
 def create_condition(owner, parcels):
     '''Create a conditional array to filter the parcels dataframe for an ownership
@@ -28,7 +29,7 @@ def create_condition(owner, parcels):
                     lb = lb & ~parcels['true_owner2'].str.contains(owner2,na=False)
         lb = (lb)
             
-    return lb
+    return lb.fillna(False)
 
 def assign_ownership(parcels,categories):
     '''Assign ownership in the categories to parcels.'''
@@ -45,14 +46,14 @@ def assign_ownership(parcels,categories):
             b = b | create_condition(sub_owner, parcels)
 
         # set the ownership and categories in the parcels with the conditional array
-        parcels.loc[b,'test_ownership'] = category['Ownership']
-        parcels.loc[b,'test_categories'] = category['Owner Categories']
+        parcels.loc[b,'camp_ownership'] = category['Ownership']
+        parcels.loc[b,'camp_categories'] = category['Owner Categories']
 
         return category['Ownership']
     
     # add columns for new categories
-    parcels['test_ownership'] = ''
-    parcels['test_categories'] = ''
+    parcels['camp_ownership'] = ''
+    parcels['camp_categories'] = ''
     
     return categories.apply(filter_and_assign, parcels=parcels, axis=1)
 
@@ -60,22 +61,58 @@ def main():
     '''Read the parcel and categegory files and process.'''
 
     if len(sys.argv) == 2:
-        opts=json.loads(sys.argv[1])
+        parcel_file = sys.argv[1]
     else: # default for testing
-        opts = {'parcel_file': 'LAND_CustomParcels_Final_2021_Tim.csv'}
+        parcel_file = "LAND_CustomParcels_Final_2021_Tim.csv"
 
     # load test parcel data and category map
-    parcels = pd.read_csv(opts['parcel_file'])
+    path = ("/").join(parcel_file.split("/")[:-1])
+    file = parcel_file.split("/")[-1].split(".")[0]
+    ext = parcel_file.split("/")[-1].split(".")[1]
+    if ext == "json":
+        with (parcel_file, 'r') as f:
+            data = json.load(f)
+        parcels = pd.json_normalize(data['features'])
+        rename = {}
+        for col in list(parcels):
+            if 'properties' in col: rename[col] = col.lower()[11:]
+        parcels = parcels.rename(columns=rename)
+    elif ext == "csv":
+        parcels = pd.read_csv(parcel_file, low_memory=False)
     categories = pd.read_csv('OwnershipMap.csv')
 
     owner_filters = assign_ownership(parcels,categories)
     
-    for owner_filter in owner_filters:
-        print((
-            f"{owner_filter} "
-            f"original {len(parcels[parcels['Ownership']==owner_filter])} "
-            f"test {len(parcels[parcels['test_ownership']==owner_filter])}"
-        ))
+    # give feedback
+    #for owner_filter in owner_filters:
+    #    print((
+    #        f"{owner_filter} "
+    #        f"original {len(parcels[parcels['Ownership']==owner_filter])} "
+    #        f"test {len(parcels[parcels['test_ownership']==owner_filter])}"
+    #    ))
+
+    if ext == "json":
+
+        def restructure(row):
+            properties = {}
+            for col in rename:
+                if 'properties' in col:
+                    prop = rename[col].upper() if rename[col] not in ['shape_area','shape_length'] else rename[col]
+                    properties[prop] = row[rename[col]]
+            properties['camp_ownership'] = row['camp_ownership']
+            properties['camp_categories'] = row['camp_categories']
+            return {
+                "type": "Feature",
+                "properties": properties,
+                "geometry": {"type": row['geometry.type'], "coordinates": row['geometry.coordinates']}
+            }
+
+        data['features'] = parcels.apply(restructure, axis=1).tolist()
+        with open(f"../download/mdc_parcels_camp_ownership.json", "w") as json_file:
+            json.dump(data, json_file)
+
+    elif ext == "csv":
+        parcels.to_csv(f"{path}\\ownership_{file}.csv", index=False)
 
 if __name__ == "__main__":
     main()
